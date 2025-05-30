631b81771ec9bf87b9a6d7f78142fd8eed5d6531
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2025-05-30 11:38:04 +0300

    fixup hole tags

--- include/recs/access.hpp
+++ include/recs/access.hpp
@@ -183,31 +183,19 @@ QueryIterator<ReadAccesses, WriteAccesses, WithAccesses>::QueryIterator(
     if (m_chunk_index >= chunk_count)
         return;
 
-    EntitiesChunk const *chunk = m_range.m_chunks[m_chunk_index];
     assert(m_entity_index < EntitiesChunk::s_max_entities);
-    EntityId const id = chunk->m_ids[m_entity_index];
-    if (id == EntityId{})
+    while (m_chunk_index < chunk_count)
     {
-        while (m_chunk_index < chunk_count)
+        EntitiesChunk const *chunk = m_range.m_chunks[m_chunk_index];
+        if (chunk->m_ids[m_entity_index] != EntityId{})
+            break;
+        HoleTag const *tag = chunk->holeTag(m_entity_index);
+        assert(tag->skip_backward == 1);
+        m_entity_index += tag->skip_forward;
+        if (m_entity_index >= EntitiesChunk::s_max_entities)
         {
-            if (m_entity_index == EntitiesChunk::s_max_entities)
-            {
-                m_entity_index = 0;
-                m_chunk_index++;
-                if (m_chunk_index == chunk_count)
-                    break;
-            }
-            EntitiesChunk const *chunk = m_range.m_chunks[m_chunk_index];
-            if (chunk->m_ids[m_entity_index] != EntityId{})
-                break;
-            HoleTag const *tag = chunk->holeTag(m_entity_index);
-            assert(tag->skip_backward == 1);
-            m_entity_index += tag->skip_forward;
-            if (m_entity_index >= EntitiesChunk::s_max_entities)
-            {
-                m_entity_index = 0;
-                m_chunk_index++;
-            }
+            m_entity_index = 0;
+            m_chunk_index++;
         }
     }
 
@@ -222,11 +210,11 @@ template <typename ReadAccesses, typename WriteAccesses, typename WithAccesses>
 QueryIterator<ReadAccesses, WriteAccesses, WithAccesses> &QueryIterator<
     ReadAccesses, WriteAccesses, WithAccesses>::operator++()
 {
+    m_entity_index++;
     size_t const chunk_count = m_range.m_chunks.size();
     while (m_chunk_index < chunk_count)
     {
-        m_entity_index++;
-        if (m_entity_index == EntitiesChunk::s_max_entities)
+        if (m_entity_index >= EntitiesChunk::s_max_entities)
         {
             m_entity_index = 0;
             m_chunk_index++;
@@ -239,11 +227,6 @@ QueryIterator<ReadAccesses, WriteAccesses, WithAccesses> &QueryIterator<
         HoleTag const *tag = chunk->holeTag(m_entity_index);
         assert(tag->skip_backward == 1);
         m_entity_index += tag->skip_forward;
-        if (m_entity_index >= EntitiesChunk::s_max_entities)
-        {
-            m_entity_index = 0;
-            m_chunk_index++;
-        }
     }
     if (m_chunk_index < chunk_count)
         m_current_entity = EntityType{ChunkEntityRef{
